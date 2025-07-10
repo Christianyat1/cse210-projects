ea47bc4c2120cd031683ff9caa8647697eecb2d0
Christianyat1 <christianyatcaal123@gmail.com>
2025-07-10 17:47:59 -0600

    Changes made to the EXc 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random anyNumber = new Random();
+        int number = anyNumber.Next(1, 101);
+        int numGuess = -1;
+        while (numGuess != number)
+        {
+            Console.Write("What is your Guess? ");
+            numGuess = int.Parse(Console.ReadLine());
+            if (number > numGuess)
+            {
+                Console.WriteLine("higher");
+
+            }
+            else if (number < numGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Your Guessed it!" );
+            }
+        }
+        
     }
 }
